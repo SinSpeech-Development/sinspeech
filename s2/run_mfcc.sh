5281df8d506e36cb93bb8835405e48f19f3a5174
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2022-01-26 18:21:34 +0530

    mfcc feature extraction

--- s2/run_mfcc.sh
+++ s2/run_mfcc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 stage=0
-LOG_LOCATION=`pwd`/../logs
+LOG_LOCATION=`pwd`/logs
 
 if [ ! -d "$LOG_LOCATION" ]; then
   mkdir -p $LOG_LOCATION
@@ -22,12 +22,16 @@ if [ $stage -le 0 ]; then
     # Making spk2utt files
     utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
     utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
+    utils/utt2spk_to_spk2utt.pl data/valid/utt2spk > data/valid/spk2utt
     
     utils/validate_data_dir.sh data/train --no-feats
     utils/fix_data_dir.sh data/train
 
     utils/validate_data_dir.sh data/test --no-feats
     utils/fix_data_dir.sh data/test
+
+    utils/validate_data_dir.sh data/valid --no-feats
+    utils/fix_data_dir.sh data/valid
 fi
 
 if [ $stage -le 1 ]; then
@@ -47,6 +51,13 @@ if [ $stage -le 1 ]; then
     steps/compute_cmvn_stats.sh data/test exp/make_mfcc/test $mfccdir
     echo
     echo "===== END: Test Set MFCC feature extraction ====="
+
+    echo "===== BEGIN : Valid Set MFCC feature extraction ====="    
+    echo
+    steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/valid exp/make_mfcc/valid $mfccdir
+    steps/compute_cmvn_stats.sh data/valid exp/make_mfcc/valid $mfccdir
+    echo
+    echo "===== END: Valid Set MFCC feature extraction ====="
 
 fi
 
